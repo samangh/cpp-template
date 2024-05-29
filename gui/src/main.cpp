2b156f21cea667d3d8a8beb2ee5a2a5293d42b79
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2024-05-29 15:09:50 +0100

    Update libsg and imgui demo

--- gui/src/main.cpp
+++ gui/src/main.cpp
@@ -12,8 +12,8 @@ int main(int, char**)
 
     sg::imgui::IImGuiWrapper::on_start_t start = [&]() { };
     sg::imgui::IImGuiWrapper::on_end_t end = [&]() { };
-    sg::imgui::IImGuiWrapper::on_iteration_t ter = [&](bool &done) { };
-    sg::imgui::ImGuiWrapper_Sdl2_OpenGl3 wrap = sg::imgui::ImGuiWrapper_Sdl2_OpenGl3(start, end, ter);
+    sg::imgui::IImGuiWrapper::on_iteration_t ter = [&](bool &done) {ImGui::ShowDemoWindow(); };
+    sg::imgui::ImGuiWrapper_Sdl2_OpenGl3 wrap = sg::imgui::ImGuiWrapper_Sdl2_OpenGl3(start, end, ter, sg::imgui::ConfigFlags::Docking);
 
     wrap.start(fmt::format("{} v{}", PROJECT_DESCRIPTION, PROJECT_VERSION));
 
