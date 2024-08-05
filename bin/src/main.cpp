debd1c8fa88300af39e968891e1978d60ad2c6ee
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2024-08-05 09:18:28 +0100

    Thread_pool test

--- bin/src/main.cpp
+++ bin/src/main.cpp
@@ -1,4 +1,5 @@
-#include <sg/file_writer.h>
+#include <iostream>
+#include <thread_pool/thread_pool.h>
 #include <memory>
 
 // Un-commonet when running a GUI (to hide Windows terminal)
@@ -7,9 +8,31 @@
 // #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
 // #endif
 
+
 int main(int, char **) {
-    sg::file_writer writer;
-    writer.start("test.txt", nullptr, nullptr, nullptr, 200);
-    writer.write_line_async("TEST");
-    writer.stop();
+    std::function<int(int)> f = [](int no){
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::cout << no <<std::endl;
+        return no;
+    };
+
+    std::function<int(int)> f2 = [](int no){
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::cout << "no:" << no <<std::endl;
+        return no;
+    };
+
+    dp::thread_pool pool(4);
+    std::vector<std::future<int>> vec;
+
+    for (int i=0; i<100; ++i)    
+        vec.emplace_back(pool.enqueue(f, i));
+
+    vec[0].get();
+    pool.clear_tasks();
+
+    for (int i=0; i<10; ++i)
+        vec.emplace_back(pool.enqueue(f2, i));
+
+    return 0;
 }
